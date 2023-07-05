73587a0a5ff3b2bbd0e43e6ee0c7627b77707e1c
Yohanes Cahyadi <yohanescahyadi59@gmail.com>
2023-07-06 00:37:17 +0700

    memperbaiki tabel diagnosa dan hasil

--- database/migrations/create_diagnosa_table.php
+++ database/migrations/create_diagnosa_table.php
@@ -1,10 +1,12 @@
 <?php
 $tableDiagnosa = "CREATE TABLE IF NOT EXISTS diagnosa(
-	kode varchar(10) PRIMARY KEY NOT NULL,
+	id int(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
+	kode_diagnosa varchar(100) NOT NULL,
 	user_id int(11) NOT NULL,
 	kode_gejala varchar(10) NOT NULL,
 	created_at timestamp NOT NULL DEFAULT current_timestamp(),
 	updated_at timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+	INDEX (kode_diagnosa),
 	FOREIGN KEY (user_id) REFERENCES users(id),
 	FOREIGN KEY (kode_gejala) REFERENCES gejala(kode)
 )";
--- database/migrations/create_hasil_table.php
+++ database/migrations/create_hasil_table.php
@@ -1,11 +1,11 @@
 <?php
 $tableHasil = "CREATE TABLE IF NOT EXISTS hasil(
 	id int(11) PRIMARY KEY NOT NULL AUTO_INCREMENT,
-	kode_diagnosa varchar(10) NOT NULL,
+	kode_diagnosa varchar(100) NOT NULL,
 	kode_kerusakan varchar(10) NOT NULL,
 	nama_kerusakan varchar(50) NOT NULL,
 	created_at timestamp NOT NULL DEFAULT current_timestamp(),
 	updated_at timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
-	FOREIGN KEY (kode_diagnosa) REFERENCES diagnosa(kode)
+	FOREIGN KEY (kode_diagnosa) REFERENCES diagnosa(kode_diagnosa)
 )";
 $createTable = mysqli_query($conn, $tableHasil);
