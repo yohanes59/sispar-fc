fa2ec90f47e82fa6d49ce58b3358eabec7f6f9b2
Yohanes Cahyadi <yohanescahyadi59@gmail.com>
2023-07-14 20:35:57 +0700

    pindahin css ke asset & tambah teks di welcome

--- views/pages/home/welcome.php
+++ views/pages/home/welcome.php
@@ -8,6 +8,8 @@
             <p style="margin-bottom: 1rem; margin-top:0;">sistem pakar adalah program komputer yang menggunakan pengetahuan yang telah dikumpulkan dari seorang pakar manusia dalam bidang tertentu untuk memberikan solusi, rekomendasi, atau diagnosis dalam suatu domain spesifik.</p>
             <h5 class="card-title">Forward Chaining</h5>
             <p style="margin-bottom: 1rem; margin-top:0;">metode forward chaining adalah pencarian maju yang di mulai dari beberapa fakta-fakta dengan mencari pedoman yang sesuai dengan dugaan/hipotesis yang muncul menuju suatu hasil / kesimpulan.</p>
+            <h5 class="card-title">Batasan Masalah :</h5>
+            <p style="margin-bottom: 1rem; margin-top:0;">Sistem pakar ini hanya terbatas untuk mesin jahit portable dan mesin jahit hitam.</p>
         </div>
     </div>
     <div class="card bg-light mb-3">
@@ -21,72 +23,3 @@
         </div>
     </div>
 </div>
-
-<style>
-    /* Extra Large devices (Laptops and Desktops) */
-    @media only screen and (min-width: 1201px) and (max-width: 1600px) {
-        .card-container {
-            display: flex;
-            justify-content: space-between;
-            gap: 1rem;
-        }
-
-        .card {
-            width: 30rem;
-            margin: 0;
-        }
-    }
-
-    .mb-3 {
-        margin-bottom: 1rem !important;
-    }
-
-    .bg-light {
-        background-color: #f8f9fa !important;
-    }
-
-    .card {
-        max-width: 40rem;
-        margin: 0 auto;
-        position: relative;
-        display: -webkit-box;
-        display: -ms-flexbox;
-        display: flex;
-        -webkit-box-orient: vertical;
-        -webkit-box-direction: normal;
-        -ms-flex-direction: column;
-        flex-direction: column;
-        min-width: 0;
-        word-wrap: break-word;
-        background-color: #fff;
-        background-clip: border-box;
-        border: 1px solid rgba(0, 0, 0, .125);
-        border-radius: 0.25rem;
-    }
-
-    .card-header {
-        padding: 0.75rem 1.25rem;
-        margin-bottom: 0;
-        background-color: rgba(0, 0, 0, .03);
-        border-bottom: 1px solid rgba(0, 0, 0, .125);
-    }
-
-    .card-body {
-        -webkit-box-flex: 1;
-        -ms-flex: 1 1 auto;
-        flex: 1 1 auto;
-        padding: 1.25rem;
-    }
-
-    .card-title {
-        margin-bottom: 0.75rem;
-    }
-
-    h5 {
-        margin-bottom: 0.5rem;
-        font-weight: 500;
-        line-height: 1.2;
-        margin-top: 0;
-        font-size: 1.25rem;
-    }
-</style>
