cbbf23cc775fda230c0d1fbf1228c15c81147472
Yohanes Cahyadi <yohanescahyadi59@gmail.com>
2023-07-14 20:44:03 +0700

    tambah kolom solusi di table kerusakan

--- database/migrations/create_kerusakan_table.php
+++ database/migrations/create_kerusakan_table.php
@@ -2,6 +2,7 @@
 $tableKerusakan = "CREATE TABLE IF NOT EXISTS kerusakan(
 	kode varchar(10) PRIMARY KEY NOT NULL,
 	nama varchar(50) NOT NULL,
+	solusi varchar(50) NOT NULL,
 	created_at timestamp NOT NULL DEFAULT current_timestamp(),
 	updated_at timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 )";
--- database/seeders/kerusakan_seeder.php
+++ database/seeders/kerusakan_seeder.php
@@ -11,37 +11,44 @@ function check_data_kerusakan($data)
 $data = [
     [
         'kode' => 'KK-01',
-        'nama' => 'Tension kendor'
+        'nama' => 'Tension kendor',
+        'solusi' => 'Solusi 1',
     ],
     [
         'kode' => 'KK-02',
-        'nama' => 'Tension kencang'
+        'nama' => 'Tension kencang',
+        'solusi' => 'Solusi 2',
     ],
     [
         'kode' => 'KK-03',
-        'nama' => 'Jarum bengkok dan terbalik'
+        'nama' => 'Jarum bengkok dan terbalik',
+        'solusi' => 'Solusi 3',
     ],
     [
         'kode' => 'KK-04',
-        'nama' => 'Jarum tumpul'
+        'nama' => 'Jarum tumpul',
+        'solusi' => 'Solusi 4',
     ],
     [
         'kode' => 'KK-05',
-        'nama' => 'Gigi tumpul'
+        'nama' => 'Gigi tumpul',
+        'solusi' => 'Solusi 5',
     ],
     [
         'kode' => 'KK-06',
-        'nama' => 'Gigi rusak'
+        'nama' => 'Gigi rusak',
+        'solusi' => 'Solusi 6',
     ],
 ];
 
 foreach ($data as $item) {
     $kode = $item['kode'];
     $nama = $item['nama'];
+    $solusi = $item['solusi'];
     $check_query = check_data_kerusakan($nama);
 
     if (mysqli_num_rows($check_query) == 0) {
-        $insert_query = "INSERT INTO kerusakan(kode, nama, created_at, updated_at) VALUES ('$kode', '$nama', NOW(), NOW())";
+        $insert_query = "INSERT INTO kerusakan(kode, nama, solusi, created_at, updated_at) VALUES ('$kode', '$nama', '$solusi', NOW(), NOW())";
         $seeder = mysqli_query($conn, $insert_query);
     }
 }
