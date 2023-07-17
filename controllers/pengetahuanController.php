7bd09d1fc6cafb0749ae2b895f14a5260eedb3c5
Yohanes Cahyadi <yohanescahyadi59@gmail.com>
2023-07-17 13:09:08 +0700

    buat validasi input saat tambah & edit pengetahuan

--- controllers/pengetahuanController.php
+++ controllers/pengetahuanController.php
@@ -18,6 +18,16 @@ class pengetahuanController extends controller
         include './views/pages/home/pengetahuan/index.php';
     }
 
+    function validate_input($kerusakan, $gejala)
+    {
+        if ((empty(trim($kerusakan)) && empty(trim($gejala))) || empty(trim($kerusakan)) || empty(trim($gejala))) {
+            $this->sweetalert('error', 'input required!', 'Data pengetahuan harus di pilih');
+            return false;
+        } else {
+            return true;
+        }
+    }
+
     public function create()
     {
         $k_Data = $this->kModel->selectAll();
@@ -30,62 +40,64 @@ class pengetahuanController extends controller
         $kerusakan = @$_POST['kerusakan'];
         $gejala = @$_POST['gejala'];
 
-        $insert = $this->pModel->insert($kerusakan, $gejala);
-        if ($insert) {
-            echo "<script type='text/javascript'>
-                    Swal.fire({
+        if ($this->validate_input($kerusakan, $gejala)) {
+            $insert = $this->pModel->insert($kerusakan, $gejala);
+            if ($insert) {
+                echo "<script type='text/javascript'>
+                Swal.fire({
                     icon: 'success',
                     title: 'Data Tersimpan',
                     text: 'Data berhasil disimpan!'
                     }).then(function() {
                         location.href = '?page=home&sub=pengetahuan';
-                    });
-                </script>";
-        }
-    }
+                        });
+                        </script>";
+                    }
+                }
+            }
 
-    public function edit($kode)
-    {
-        $k_Data = $this->kModel->selectAll();
-        $g_Data = $this->gModel->selectAll();
-        $data = $this->pModel->select($kode);
-        $itemData = $this->pModel->fetch($data);
-        include './views/pages/home/pengetahuan/edit.php';
-    }
+            public function edit($kode)
+            {
+                $k_Data = $this->kModel->selectAll();
+                $g_Data = $this->gModel->selectAll();
+                $data = $this->pModel->select($kode);
+                $itemData = $this->pModel->fetch($data);
+                include './views/pages/home/pengetahuan/edit.php';
+            }
 
-    public function update()
-    {
-        $kode = $_GET['kode'];
-        $kode_kerusakan = @$_POST['kerusakan'];
-        $kode_gejala = @$_POST['gejala'];
+            public function update()
+            {
+                $kode = $_GET['kode'];
+                $kode_kerusakan = @$_POST['kerusakan'];
+                $kode_gejala = @$_POST['gejala'];
 
-        $update = $this->pModel->update($kode, $kode_kerusakan, $kode_gejala);
-        if ($update) {
-            echo "<script type='text/javascript'>
+                $update = $this->pModel->update($kode, $kode_kerusakan, $kode_gejala);
+                if ($update) {
+                    echo "<script type='text/javascript'>
                     Swal.fire({
-                    icon: 'success',
-                    title: 'Data Tersimpan',
-                    text: 'Data berhasil diubah!'
-                    }).then(function() {
-                        location.href = '?page=home&sub=pengetahuan';
-                    });
-                </script>";
-        }
-    }
+                        icon: 'success',
+                        title: 'Data Tersimpan',
+                        text: 'Data berhasil diubah!'
+                        }).then(function() {
+                            location.href = '?page=home&sub=pengetahuan';
+                            });
+                            </script>";
+                        }
+                    }
 
-    public function destroy($kode)
-    {
-        $delete = $this->pModel->delete($kode);
-        if ($delete) {
-            echo "<script type='text/javascript'>
-                Swal.fire({
-                icon: 'success',
-                title: 'Data Terhapus',
-                text: 'Data berhasil dihapus!'
-                }).then(function() {
-                    location.href = '?page=home&sub=pengetahuan';
-                });
-            </script>";
-        }
-    }
-}
+                    public function destroy($kode)
+                    {
+                        $delete = $this->pModel->delete($kode);
+                        if ($delete) {
+                            echo "<script type='text/javascript'>
+                            Swal.fire({
+                                icon: 'success',
+                                title: 'Data Terhapus',
+                                text: 'Data berhasil dihapus!'
+                                }).then(function() {
+                                    location.href = '?page=home&sub=pengetahuan';
+                                    });
+                                    </script>";
+                                }
+                            }
+                        }
