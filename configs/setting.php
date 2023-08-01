36e2c56d2fac9abc951cb9414599ac4289815423
Yohanes Cahyadi <yohanescahyadi59@gmail.com>
2023-08-01 18:02:46 +0700

    pindahin kode js agar dinamis sesuai buka halaman

--- configs/setting.php
+++ configs/setting.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 define('APP_NAME', 'Sistem Pakar Kerusakan Mesin Jahit Forward Chaining');
 
 define('REGISTER_URL', 'register');
@@ -12,4 +12,64 @@ define('WELCOME_URL', 'welcome');
 define('ATURAN_URL', 'aturan');
 define('DIAGNOSA_URL', 'diagnosa');
 define('HASIL_URL', 'hasil');
-?>
+
+define('GEJALA_SCRIPT', '
+    function confirmDeleteGejala(event, kode) {
+        event.preventDefault();
+
+        Swal.fire({
+            title: "Konfirmasi",
+            text: "Yakin ingin menghapus data ini?",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonText: "Ya, Hapus!",
+            cancelButtonText: "Batal"
+        }).then((result) => {
+            if (result.isConfirmed) {
+                let deleteUrl = "?page=" + "' . HOME_URL . '" + "&sub=" + "' . GEJALA_URL . '" + "&act=delete&kode=" + kode;
+                window.location.href = deleteUrl;
+            }
+        });
+    }
+');
+
+define('KERUSAKAN_SCRIPT', '
+function confirmDeleteKerusakan(event, kode) {
+    event.preventDefault();
+
+    Swal.fire({
+        title: "Konfirmasi",
+        text: "Yakin ingin menghapus data ini?",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonText: "Ya, Hapus!",
+        cancelButtonText: "Batal"
+    }).then((result) => {
+        if (result.isConfirmed) {
+            let deleteUrl = "?page=" + "' . HOME_URL . '" + "&sub=" + "' . KERUSAKAN_URL . '" + "&act=delete&kode=" + kode;
+            window.location.href = deleteUrl;
+        }
+    });
+}
+');
+
+define('PENGETAHUAN_SCRIPT', '
+function confirmDeletePengetahuan(event, id) {
+    event.preventDefault();
+
+    Swal.fire({
+        title: "Konfirmasi",
+        text: "Yakin ingin menghapus data ini?",
+        icon: "warning",
+        showCancelButton: true,
+        confirmButtonText: "Ya, Hapus!",
+        cancelButtonText: "Batal"
+    }).then((result) => {
+        if (result.isConfirmed) {
+            let deleteUrl = "?page=" + "' . HOME_URL . '" + "&sub=" + "' . PENGETAHUAN_URL . '" + "&act=delete&kode=" + id;
+            window.location.href = deleteUrl;
+        }
+    });
+}
+');
+
